7d36375fbc327dca58a5cf054572709677de51f2
AndreySamaev <andreysamaev@gmail.com>
2024-12-30 13:44:33 +0200

    layout added

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,5 +1,8 @@
-<div class="container">
-    <h3>{{ $heading }}</h3>
-    <h6>{{ $listing['title'] }}</h6>
-    <div>{{ $listing['description'] }}</div>
-</div>
+@extends('layout')
+@section('content')
+    <div >
+        <h3>{{ $heading }}</h3>
+        <h6>{{ $listing['title'] }}</h6>
+        <div>{{ $listing['description'] }}</div>
+    </div>
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,15 +1,18 @@
-<div class="container">
-    <h1>{{ $heading  }}</h1>
-    @if(count($listings) > 0)
-        @foreach($listings as $listing)
-            <a href="/listings/{{ $listing['id'] }}">
-                <h4>{{ $listing['title'] }}</h4>
-            </a>
-            <div>{{ $listing['description'] }}</div>
-        @endforeach
-    @else
-        <p>No listings found</p>
-    @endif
+@extends('layout')
+@section('content')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4">
+        <h1>{{ $heading  }}</h1>
+        @if(count($listings) > 0)
+            @foreach($listings as $listing)
+                <a href="/listings/{{ $listing['id'] }}">
+                    <h4>{{ $listing['title'] }}</h4>
+                </a>
+                <div>{{ $listing['description'] }}</div>
+            @endforeach
+        @else
+            <p>No listings found</p>
+        @endif
 
-</div>
+    </div>
+@endsection
 
