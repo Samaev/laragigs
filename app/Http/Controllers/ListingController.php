91e17e91641768b913d7284f84aeff915ed31c03
AndreySamaev <andreysamaev@gmail.com>
2024-12-31 11:43:41 +0200

    controller added

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    public function index() {
+        return view('listing', [
+            'listings' => Listing::all()
+        ]);
+    }
+    public function show(Listing $listing) {
+        return view('listing', [
+            'listing' => $listing
+        ]);
+    }
+}
