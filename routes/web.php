262f907976f47003b87f5751745fbbc9d406e3c4
AndreySamaev <andreysamaev@gmail.com>
2025-01-03 17:43:03 +0200

    update added

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::post('/listings', [ListingController::class, 'store']);
 
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
