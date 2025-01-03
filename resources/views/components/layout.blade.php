512132cbb396d4c2bf191cc5e41cd1cfa9e805c5
AndreySamaev <andreysamaev@gmail.com>
2025-01-03 15:20:48 +0200

    fix extra syntax

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,17 +30,17 @@
 <body class="mb-48">
 <nav class="flex justify-between items-center mb-4">
     <a href="/">
-        <img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" />
+        <img src="{{ asset('images/logo.png') }}" alt="" class="w-24 logo"/>
     </a>
     <ul class="flex space-x-6 mr-6 text-lg">
         <li>
-            <a href="register.html" class="hover:text-laravel" >
+            <a href="register.html" class="hover:text-laravel">
                 <i class="fa-solid fa-user-plus"></i>
                 Register
-            </a >
+            </a>
         </li>
         <li>
-            <a href="login.html" class="hover:text-laravel" >
+            <a href="login.html" class="hover:text-laravel">
                 <i class="fa-solid fa-arrow-right-to-bracket"></i>
                 Login
             </a>
@@ -58,10 +58,11 @@
     <a
         href="/listings/create"
         class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-    >Post Job</a
     >
+        Post Job
+    </a>
 </footer>
 
-<x-flash-message />
+<x-flash-message/>
 </body>
 </html>
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -12,5 +12,5 @@
         @endif
 
     </div>
-</x-layout>>
+</x-layout>
 
