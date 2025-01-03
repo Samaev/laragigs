d56444ce6686c10be6d299e320ce04cc072c8159
AndreySamaev <andreysamaev@gmail.com>
2025-01-03 12:03:10 +0200

    search form added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,15 +33,16 @@
     </a>
     <ul class="flex space-x-6 mr-6 text-lg">
         <li>
-            <a href="register.html" class="hover:text-laravel"
-            ><i class="fa-solid fa-user-plus"></i> Register</a
-            >
+            <a href="register.html" class="hover:text-laravel" >
+                <i class="fa-solid fa-user-plus"></i>
+                Register
+            </a >
         </li>
         <li>
-            <a href="login.html" class="hover:text-laravel"
-            ><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                Login</a
-            >
+            <a href="login.html" class="hover:text-laravel" >
+                <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                Login
+            </a>
         </li>
     </ul>
 </nav>
