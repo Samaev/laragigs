e81520624c358bad23a167ae97839596d21faba2
AndreySamaev <andreysamaev@gmail.com>
2025-01-03 18:17:32 +0200

    register blade added

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,15 @@ Route::get('/', [ListingController::class, 'index']);
 Route::get('/listings/create', [ListingController::class, 'create']);
 
 Route::post('/listings', [ListingController::class, 'store']);
-
+//Show Edit Form Listing
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
-
+//Update Listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
-
+//Delete Listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
-
+//Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
+//Show Register/Create Form
 Route::get('/register', [UserController::class, 'create']);
 
 
